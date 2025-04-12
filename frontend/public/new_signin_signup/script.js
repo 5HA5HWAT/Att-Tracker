6ad7459cff2ed22547b812539bcd7273f50d9a37
Shashwat Mishra <shashwatm661@gmail.com>
2025-04-12 23:46:29 +0530

    ok2

--- frontend/public/new_signin_signup/script.js
+++ frontend/public/new_signin_signup/script.js
@@ -1,5 +1,4 @@
 function toggleForms(type) {
-<<<<<<< HEAD
   const signInForm = document.getElementById('signInForm');
   const signUpForm = document.getElementById('signUpForm');
   const signInBtn = document.getElementById('signInBtn');
@@ -9,30 +8,15 @@ function toggleForms(type) {
     signInForm.classList.remove('hidden');
     signUpForm.classList.add('hidden');
     signInBtn.classList.add('border-purple-600');
+    signInBtn.classList.remove('border-transparent');
+    signUpBtn.classList.add('border-transparent');
     signUpBtn.classList.remove('border-purple-600');
   } else {
     signUpForm.classList.remove('hidden');
     signInForm.classList.add('hidden');
     signUpBtn.classList.add('border-purple-600');
+    signUpBtn.classList.remove('border-transparent');
+    signInBtn.classList.add('border-transparent');
     signInBtn.classList.remove('border-purple-600');
   }
 }
-=======
-    const signInForm = document.getElementById('signInForm');
-    const signUpForm = document.getElementById('signUpForm');
-    const signInBtn = document.getElementById('signInBtn');
-    const signUpBtn = document.getElementById('signUpBtn');
-
-    if (type === 'signIn') {
-      signInForm.classList.remove('d-none');
-      signUpForm.classList.add('d-none');
-      signInBtn.classList.add('active');
-      signUpBtn.classList.remove('active');
-    } else {
-      signUpForm.classList.remove('d-none');
-      signInForm.classList.add('d-none');
-      signUpBtn.classList.add('active');
-      signInBtn.classList.remove('active');
-    }
-  }
->>>>>>> f460b295732004268ddb69d49ee0c37abc92c4ac
