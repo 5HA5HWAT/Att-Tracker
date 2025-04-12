7fed9e45796e0ba529486e592cd31d54b7052729
Shashwat Mishra <shashwatm661@gmail.com>
2025-04-12 22:13:59 +0530

    frontend

---
+++ frontend/public/new_signin_signup/script.js
@@ -0,0 +1,18 @@
+function toggleForms(type) {
+  const signInForm = document.getElementById('signInForm');
+  const signUpForm = document.getElementById('signUpForm');
+  const signInBtn = document.getElementById('signInBtn');
+  const signUpBtn = document.getElementById('signUpBtn');
+
+  if (type === 'signIn') {
+    signInForm.classList.remove('hidden');
+    signUpForm.classList.add('hidden');
+    signInBtn.classList.add('border-purple-600');
+    signUpBtn.classList.remove('border-purple-600');
+  } else {
+    signUpForm.classList.remove('hidden');
+    signInForm.classList.add('hidden');
+    signUpBtn.classList.add('border-purple-600');
+    signInBtn.classList.remove('border-purple-600');
+  }
+}
